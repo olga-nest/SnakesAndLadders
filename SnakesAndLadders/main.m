92871641323c4b3020ee39d149e0c693a479fb0c
olga-nest <olga.w.nesterova@gmail.com>
2017-10-07 23:12:23 -0700

    Check user's input, call player

--- SnakesAndLadders/main.m
+++ SnakesAndLadders/main.m
@@ -1,24 +1,29 @@
 #import <Foundation/Foundation.h>
+#import "Player.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
         BOOL gameOn = YES;
         
+        Player *player = [[Player alloc]init];
+        
         char answerCString;
-        printf("To start the game print roll");
+        printf("To start the game print roll \n > ");
         fgets(&answerCString, 255, stdin);
         NSString *result = [NSString stringWithCString:&answerCString
                                               encoding:NSUTF8StringEncoding];
-        
         //removes new line and white spaces
         NSCharacterSet *resultSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
         NSString *usersInp = [result stringByTrimmingCharactersInSet:(NSCharacterSet *)resultSet];
-        
-        
+    
         while (gameOn == YES) {
-            
+            if ([usersInp isEqual: @"roll"]) {
+                [player roll];
+            }
+
         }
+
     }
     return 0;
 }

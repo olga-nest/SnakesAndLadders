004b4749bd4611e547b9b7874af5f34f1082fee0
olga-nest <olga.w.nesterova@gmail.com>
2017-10-07 21:59:25 -0700

    Grab the user's input

--- SnakesAndLadders/main.m
+++ SnakesAndLadders/main.m
@@ -1,17 +1,24 @@
-//
-//  main.m
-//  SnakesAndLadders
-//
-//  Created by Olga on 10/7/17.
-//  Copyright © 2017 Olga Nesterova. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        
+        BOOL gameOn = YES;
+        
+        char answerCString;
+        printf("To start the game print roll");
+        fgets(&answerCString, 255, stdin);
+        NSString *result = [NSString stringWithCString:&answerCString
+                                              encoding:NSUTF8StringEncoding];
+        
+        //removes new line and white spaces
+        NSCharacterSet *resultSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
+        NSString *usersInp = [result stringByTrimmingCharactersInSet:(NSCharacterSet *)resultSet];
+        
+        
+        while (gameOn == YES) {
+            
+        }
     }
     return 0;
 }

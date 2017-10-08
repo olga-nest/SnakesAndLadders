42f5a470d4d9c3c9630de623c3b0c91305325af7
olga-nest <olga.w.nesterova@gmail.com>
2017-10-08 09:35:29 -0700

    Add currentSquare value

--- SnakesAndLadders/Player.m
+++ SnakesAndLadders/Player.m
@@ -2,9 +2,21 @@
 
 @implementation Player
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _currentSquare = 0;
+    }
+    return self;
+}
+
 -(void)roll {
-    int diceValue = (arc4random_uniform(6) + 1);
-    NSLog(@"Your result is %d", diceValue);
+    int diceRandomValue = (arc4random_uniform(6) + 1);
+    NSLog(@"Your result is %d", diceRandomValue);
+    
+    self.currentSquare = self.currentSquare + diceRandomValue;
+    NSLog(@"New current square is: %ld", (long)self.currentSquare);
 }
 
 @end

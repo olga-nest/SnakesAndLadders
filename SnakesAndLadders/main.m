48a1bb290b0755ca467bc069e63af4d5a6456e30
olga-nest <olga.w.nesterova@gmail.com>
2017-10-07 21:50:43 -0700

    Initial Commit

---
+++ SnakesAndLadders/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SnakesAndLadders
+//
+//  Created by Olga on 10/7/17.
+//  Copyright © 2017 Olga Nesterova. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}

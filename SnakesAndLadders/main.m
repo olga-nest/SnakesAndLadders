b659364adc8133a51be04030a4dc8c392e107a46
olga-nest <olga.w.nesterova@gmail.com>
2017-10-08 09:27:15 -0700

    Fix crash on roll

--- SnakesAndLadders/main.m
+++ SnakesAndLadders/main.m
@@ -8,18 +8,19 @@ int main(int argc, const char * argv[]) {
         
         Player *player = [[Player alloc]init];
         
-        char answerCString;
+        int buf = 50;
+        char answerCString[buf];
         printf("To start the game print roll \n > ");
-        fgets(&answerCString, 255, stdin);
-        NSString *result = [NSString stringWithCString:&answerCString
+        fgets(answerCString, buf, stdin);
+        NSString *result = [NSString stringWithCString:answerCString
                                               encoding:NSUTF8StringEncoding];
         //removes new line and white spaces
         NSCharacterSet *resultSet = [NSCharacterSet whitespaceAndNewlineCharacterSet];
         NSString *usersInp = [result stringByTrimmingCharactersInSet:(NSCharacterSet *)resultSet];
-    
+     
         while (gameOn == YES) {
             if ([usersInp isEqual: @"roll"]) {
-                [player roll];
+               [player roll];
             }
 
         }

01f48289f701dde639e625b3698ca0331e979050
olga-nest <olga.w.nesterova@gmail.com>
2017-10-08 18:34:54 -0700

    Handle snake case

--- SnakesAndLadders/Player.m
+++ SnakesAndLadders/Player.m
@@ -8,7 +8,7 @@
     if (self) {
         _currentSquare = 0;
         _gameLogic = [NSDictionary new];
-        _gameLogic = @{@5 : @10, @9: @12, @17: @15, @25 : @50, @33 : @11};
+        _gameLogic = @{@5 : @10, @9: @12, @17: @15, @25 : @50, @33 : @11, @51 : @53, @57 : @32, @58 :@24, @60 : @2, @77 : @75, @81 : @90, @90 : @50};
         
     }
     return self;
@@ -19,21 +19,22 @@
     NSLog(@"Your result is %d", diceRandomValue);
     
     self.currentSquare = self.currentSquare + diceRandomValue;
-//    NSLog(@"New current square is: %ld", (long)self.currentSquare);
     
     NSNumber *currentSquareNumber = [NSNumber numberWithInteger:self.currentSquare];
     if ([self.gameLogic objectForKey:currentSquareNumber] != nil) {
-        self.currentSquare = [[self.gameLogic objectForKey:currentSquareNumber] integerValue];
-        NSLog(@"Oops! snake! New current square is: %ld", (long)self.currentSquare);
+     //   self.currentSquare = [[self.gameLogic objectForKey:currentSquareNumber] integerValue];
+        if (self.currentSquare > [[self.gameLogic objectForKey:currentSquareNumber] integerValue]) {
+            self.currentSquare = [[self.gameLogic objectForKey:currentSquareNumber] integerValue];
+            NSLog(@"Oops! Snake! Now you are on square: %ld", (long)self.currentSquare);
+        } else  if ((self.currentSquare < [[self.gameLogic objectForKey:currentSquareNumber] integerValue])) {
+            self.currentSquare = [[self.gameLogic objectForKey:currentSquareNumber] integerValue];
+            NSLog(@"Yay! Ladder! Now you are on square %ld", (long)self.currentSquare);
+        }
         self.currentSquare = self.currentSquare;
+        
     } else {
-       NSLog(@"New current square is: %ld", (long)self.currentSquare);
+       NSLog(@"Now you are on square %ld", (long)self.currentSquare);
     }
 }
 
 @end
-
-
-
-//dict = @{ k1 : o1, k2 : o2, k3 : o3 };
-//[songDict setObject:@"2 minutes" forKey:@"durationKey"];
